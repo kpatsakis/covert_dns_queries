fb69867639256a1972e5564f21e53cb159f46752
cp <kpatsa@gmail.com>
2018-11-26 22:57:32 +0200

    All included :)

---
+++ README.md
@@ -0,0 +1,13 @@
+# Covert DNS queries
+These are some bash scripts to allow testing of covert DNS queries using DNS over TLS and DNS over HTTPS.
+## Depedencies
+To execute the tests you should have installed the following:
+* tcpdump
+* [pydig](https://github.com/shuque/pydig)
+* tshark
+* subversion
+* pdflatex (for creating the figures)
+* texlive-pictures (for creating the figures)
+
+# Acknowledgments
+This work was supported by the European Commission under the Horizon 2020 Programme (H2020), as part of the project [YAKSHA](http://project-yaksha.eu) (Grant Agreement no. 780498) and is based upon work from COST Action [CRYPTACUS](https://www.cryptacus.eu/), supported by COST (European Cooperation in Science and Technology).
---
+++ analyse_tls.sh
@@ -0,0 +1,114 @@
+#!/bin/bash
+
+# a function to display a progress bar
+function ProgressBar {
+  let _progress=(${1}*100/${2}*100)/100
+  let _done=(${_progress}*4)/10
+  let _left=40-$_done
+  _fill=$(printf "%${_done}s")
+  _empty=$(printf "%${_left}s")
+  printf "\rProgress : [${_fill// /\#}${_empty// /-}] ${_progress}%%"
+}
+
+tests=1000
+myip="192.168.2.15"
+
+# download the needed files if they are not present
+if [ ! -f ./goz.txt ]; then
+  svn checkout https://github.com/andrewaeva/DGA/trunk/dga_wordlists
+  mv dga_wordlists/*txt .
+  rm open* others* dga_wordlists/ -Rf
+fi
+
+if [ ! -f ./top-1m.csv ]; then
+  wget http://s3.amazonaws.com/alexa-static/top-1m.csv.zip
+  unzip top-1m.csv.zip
+  rm top-1m.csv.zip
+fi
+
+echo "========================"
+echo "Working with Alexa"
+echo "========================"
+echo
+
+tcpdump -i enp3s0 -w alexa_tls.pcap &
+sleep 3
+
+#get the first domains from alexa
+head -n $tests top-1m.csv|awk -F',' '{print $2}' > top1K
+cnt=0
+while read p; do
+  ./pydig @185.49.141.38 +tls=auth +tls_hostname=getdnsapi.net "$p" > /dev/null
+  let cnt+=1
+  if [ $(( $cnt  % 50 )) -eq 0 ]; then
+    sleep 2
+  fi
+  ProgressBar ${cnt} ${tests}
+done < top1K
+rm top1K
+
+# find the PID of tcpdump
+pid=$(sudo ps -e | pgrep tcpdump)
+# now kill tcpdump
+sleep 3
+sudo kill -2 $pid
+
+# now capture traffic for domains from DGAs
+echo
+echo "========================"
+echo "Working with DGA domains"
+echo "========================"
+echo
+
+for f in *.txt
+do
+  fname=${f%.*}
+  echo "Working with $fname DGA"
+  sfx="_tls.pcap"
+  tcpdump -i enp3s0 -w "$fname$sfx" &
+  sleep 3
+  cnt=0
+  head -n $tests $f > dgatmp
+  while read p; do
+    ./pydig @185.49.141.38 +tls=auth +tls_hostname=getdnsapi.net "$p" > /dev/null
+    let cnt+=1
+    if [ $(( $cnt  % 50 )) -eq 0 ]; then
+      sleep 3
+    fi
+    ProgressBar ${cnt} ${tests}
+  done < dgatmp
+
+  # find the PID of tcpdump
+  pid=$(sudo ps -e | pgrep tcpdump)
+
+  # now kill tcpdump
+  sleep 3
+  sudo kill -2 $pid
+done
+
+rm dgatmp -f
+
+# now create the figures
+for f in *.pcap
+do
+  fname=${f%.*}
+  echo "packet,length">$fname.csv
+  tshark -r $f -2 -R "ip.dst == $myip" -T fields -E separator=,, -e ip.src -e ip.dst -e frame.len -e _ws.col.Protocol -e _ws.col.Info|grep "Application"|awk -F',' -v OFS=',' '{print NR,$3}'>> $fname.csv
+  cp template.tex_ $fname.tex
+  sed -i "s/changeme/$fname/g" $fname.tex
+  pdflatex $fname.tex
+done
+
+# clean up the mess
+# leave only the csv and pcap files
+exts="aux bbl blg brf idx ilg ind lof log lol lot out toc synctex.gz fls fdb_latexmk txt"
+for ext in $exts; do
+  rm -f *.$ext
+done
+
+mkdir -p figures
+mv *pdf figures
+mkdir -p backupfiles
+mv *pcap backupfiles
+mv *csv backupfiles
+mv *tex backupfiles
